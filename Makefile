72c27a50e72975f01c82704f447176f937291586
Daniel <d.m-2006mail.ru@MacBook-Air-Daniel.local>
2026-03-17 12:56:38 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+top-vect main.cpp
+	g++ -o top-vect -Wall -Wextra -Weffct+ -std=c++14 main.cpp
---
+++ top_it_vector.hpp
@@ -0,0 +1,9 @@
+#ifndef TOP_IT_VECTOR_HPP
+#define TOP_IT_VECTOR_HPP
+template < class T >
+struct Vector {
+    private:
+    T* data_:
+    size_t size_, capacity_;
+};
+#endif
